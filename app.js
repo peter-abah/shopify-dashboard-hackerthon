31aa1d5a5eaee7417248efab67c263f91d16c5c5
Peter Abah <peterabah.ob@gmail.com>
2023-11-23 23:05:28 +0100

    Refactor js and fix UI bugs

--- app.js
+++ app.js
@@ -1,75 +1,41 @@
 "use strict";
 
-const alertsBtn = document.getElementById("alerts-btn");
-const alertsContainer = document.getElementById("alerts");
-const navMenuBtn = document.getElementById("nav-menu-btn");
-const navMenu = document.getElementById("nav-menu");
-const popups = [alertsContainer, navMenu];
-const popupBtns = [alertsBtn, navMenuBtn];
-const callout = document.getElementById("callout");
-const calloutCloseBtn = document.getElementById("callout-close-btn");
-const toggleSetupBtn = document.getElementById("toggle-setup-btn");
-const onboardingStepsList = document.getElementById("onboarding-steps");
-const onboardingStepToggleBtns = [...document.getElementsByClassName("onboarding-step-toggle")];
-const onboardingSteps = [...document.querySelectorAll(".onboarding-steps li")];
-const checkStepBtns = [...document.getElementsByClassName("check-step-btn")];
-const progressbar = document.getElementById("progess-bar");
-const progressCount = document.getElementById("progress-count");
+function app() {
+  const alertsBtn = document.getElementById("alerts-btn");
+  const alertsContainer = document.getElementById("alerts");
+  const menuBtn = document.getElementById("menu-btn");
+  const menu = document.getElementById("menu");
+  const popups = [alertsContainer, menu];
 
-addEventListenerForPopups();
-hideOnClickOutside();
+  const callout = document.getElementById("callout");
+  const calloutCloseBtn = document.getElementById("callout-close-btn");
 
-calloutCloseBtn.addEventListener("click", () => {
-  callout.classList.add("hidden");
-});
+  const toggleSetupBtn = document.getElementById("toggle-setup-btn");
+  const onboardingStepsList = document.getElementById("onboarding-steps");
+  const toggleOnboardingStepVisibiltyBtns = document.querySelectorAll(".onboarding-step-toggle");
+  const onboardingSteps = document.querySelectorAll(".onboarding-steps li");
 
+  const toggleOnboardingStepCompleteBtns = document.querySelectorAll(".check-step-btn");
+  const progressbar = document.getElementById("progess-bar");
+  const progressCount = document.getElementById("progress-count");
 
-toggleSetupBtn.addEventListener("click", () => {
-  onboardingStepsList.classList.toggle("hidden");
-  toggleSetupBtn.dataset.isOpen = toggleSetupBtn.dataset.isOpen ? '' : true;
-});
-
-onboardingStepToggleBtns.forEach((btn, index) => {
-  btn.addEventListener("click", (e) => {
-    showOnboardingStep(onboardingSteps, index);
-  });
-});
-
-checkStepBtns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    const onboardingStep = onboardingSteps[index];
-    onboardingStep.dataset.isCompleted = onboardingStep.dataset.isCompleted ? "" : true;
-
-    // [...btn.children].forEach((el) => el.classList.toggle("hidden"));
-    updateProgressBar();
-
-    const nextStepIndex = findNextUncompletedStepIndex(onboardingSteps);
-    if (nextStepIndex != -1) {
-      showOnboardingStep(onboardingSteps, nextStepIndex);
-    }
-  });
-});
-
-function addEventListenerForPopups() {
-  popupBtns.forEach((btn, index) => {
-    btn.addEventListener("click", (event) => {
-      popups.forEach((popup, popupIndex) => {
-        if ((index === popupIndex)) return;
-
-        popup.classList.add("hidden");
-      });
-
-      popups[index].classList.toggle("hidden");
-      btn.setAttribute('aria-expanded', !popups[index].classList.contains('hidden'))
-      if (!popups[index].classList.contains("hidden")) {
-        event.stopPropagation();
+  function togglePopups(event, popupBtn, popupBtnIndex) {
+    popups.forEach((popup, popupIndex) => {
+      if (popupBtnIndex === popupIndex) {
+        return;
       }
+      popup.classList.add("hidden");
     });
-  });
-}
 
-function hideOnClickOutside() {
-  document.addEventListener("click", (event) => {
+    popups[popupBtnIndex].classList.toggle("hidden");
+    const isPopupOpen = !popups[popupBtnIndex].classList.contains("hidden");
+    popupBtn.setAttribute("aria-expanded", isPopupOpen);
+    if (isPopupOpen) {
+      event.stopPropagation();
+    }
+  }
+
+  function hidePopupsOnClickOutside(event) {
     popups.forEach((element) => {
       if (!element || element.contains(event.target)) {
         return;
@@ -77,21 +43,60 @@ function hideOnClickOutside() {
 
       element.classList.add("hidden");
     });
+  }
+
+  function toggleSetup() {
+    onboardingStepsList.classList.toggle("hidden");
+
+    const isOpen = !onboardingStepsList.classList.contains("hidden");
+    toggleSetupBtn.setAttribute("aria-expanded", isOpen);
+    toggleSetupBtn.dataset.isOpen = isOpen ? "" : true;
+  }
+
+  function showOnboardingStep(onboardingStepIndex) {
+    onboardingSteps.forEach((el) => el.classList.remove("active"));
+    onboardingSteps.item(onboardingStepIndex).classList.add("active");
+  }
+
+  function toggleOnboardingStepComplete(toggleBtnIndex) {
+    const onboardingStep = onboardingSteps.item(toggleBtnIndex);
+    onboardingStep.dataset.isCompleted = onboardingStep.dataset.isCompleted ? "" : true;
+
+    updateProgressBar();
+
+    const nextStepIndex = findNextUncompletedStepIndex(onboardingSteps);
+    if (nextStepIndex != -1) {
+      showOnboardingStep(onboardingSteps, nextStepIndex);
+    }
+  }
+
+  function findNextUncompletedStepIndex(onboardingSteps) {
+    const nextStepIndex = onboardingSteps.findIndex((step) => !step.dataset.isCompleted);
+    return nextStepIndex;
+  }
+
+  function updateProgressBar() {
+    const completedStepsNo = onboardingSteps.filter((step) => step.dataset.isCompleted).length;
+    progressbar.style.width = `${completedStepsNo * 20}%`;
+    progressCount.innerText = completedStepsNo;
+  }
+
+  alertsBtn.addEventListener("click", (event) => togglePopups(event, alertsBtn, 0));
+  menuBtn.addEventListener("click", (event) => togglePopups(event, menuBtn, 1));
+  document.addEventListener("click", hidePopupsOnClickOutside);
+
+  calloutCloseBtn.addEventListener("click", () => {
+    callout.classList.add("hidden");
+  });
+
+  toggleSetupBtn.addEventListener("click", toggleSetup);
+  toggleOnboardingStepVisibiltyBtns.forEach((btn, btnIndex) => {
+    btn.addEventListener("click", () => showOnboardingStep(btnIndex));
+  });
+
+  toggleOnboardingStepCompleteBtns.forEach((btn, btnIndex) => {
+    btn.addEventListener("click", () => toggleOnboardingStepComplete(btnIndex));
   });
 }
 
-function showOnboardingStep(onboardingSteps, index) {
-  onboardingSteps.forEach((el) => el.classList.remove("active"));
-  onboardingSteps[index].classList.add("active");
-}
-
-function findNextUncompletedStepIndex(onboardingSteps) {
-  const nextStepIndex = onboardingSteps.findIndex((step) => !step.dataset.isCompleted);
-  return nextStepIndex;
-}
-
-function updateProgressBar() {
-  const completedStepsNo = onboardingSteps.filter((step) => step.dataset.isCompleted).length;
-  progressbar.style.width = `${completedStepsNo * 20}%`;
-  progressCount.innerText = completedStepsNo;
-}
+app();
